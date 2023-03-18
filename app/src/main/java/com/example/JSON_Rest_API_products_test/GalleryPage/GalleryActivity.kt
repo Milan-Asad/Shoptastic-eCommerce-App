950a4bd52b6e3874c50ee791a326b5c635f64cbd
Milan-Asad <m.milan@students.rave.ac.uk>
2023-03-18 11:05:22 +0000

    Uni FMP JSON REST API (Shopping API)
    Version 1.5
    
    Note:
    Gallery page onclick listener added
    
    18/03/2023

--- app/src/main/java/com/example/JSON_Rest_API_products_test/GalleryPage/GalleryActivity.kt
+++ app/src/main/java/com/example/JSON_Rest_API_products_test/GalleryPage/GalleryActivity.kt
@@ -1,6 +1,7 @@
 package com.example.JSON_Rest_API_products_test.GalleryPage
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -8,12 +9,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.JSON_Rest_API_products_test.LoginPage.LoginActivity
 import com.example.JSON_Rest_API_products_test.MainActivity
+import com.example.JSON_Rest_API_products_test.MensCategory
 import com.example.JSON_Rest_API_products_test.R
 import kotlinx.android.synthetic.main.activity_gallery.*
 
 class GalleryActivity : AppCompatActivity() {
 
-
+    // COMPANION OBJECT
+    companion object{
+        val INTENT_PARCELABLE = "OBJECT_INTENT"
+    }
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<GalleryAdapter.ViewHolder>? = null
@@ -22,19 +27,40 @@ class GalleryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
+        // MAKKING ARRAY FOR MENS IMAGE PAGE
+        val MensimageList = listOf<MensImages>(
+            MensImages(
+                R.raw.menstshirt,
+                "SHIRTS",
+            ),
+            MensImages(
+                R.raw.mensjacket,
+                "JACKETS",
+            )
 
-//        val recyclerView = findViewById<RecyclerView>(R.id.gallery_recyclerview)
-//        val adapter = MyAdapter(GalleryAdapter)
-//        recyclerView.adapter = adapter
+        )
 
+
+        // RECYCLERVIEW ETC STUFF
         layoutManager = LinearLayoutManager(this)
         gallery_recyclerview.layoutManager = layoutManager
-
         adapter = GalleryAdapter()
-        gallery_recyclerview.adapter = adapter
+        gallery_recyclerview.adapter = MensImageAdapter(this, MensimageList) {
+            val intent = Intent(this, MensCategory::class.java)
+            intent.putExtra(INTENT_PARCELABLE, it)
+            startActivity(intent)
+        }
+
+        /*
+        recyclerView.adapter = ImageAdapter(this, imageList){
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra(INTENT_PARCELABLE, it)
+            startActivity(intent)
+        }
+         */
 
 
-
+        //********** BOTTOM NAVIGATION BAR **********
 
         // OPENS MAIN SCREEN (LEFT)
         val ListBtn = findViewById(R.id.ListImage) as ImageView
@@ -68,3 +94,11 @@ class GalleryActivity : AppCompatActivity() {
 
     }
 }
+
+/*
+layoutManager = LinearLayoutManager(this)
+        gallery_recyclerview.layoutManager = layoutManager
+        adapter = GalleryAdapter()
+        gallery_recyclerview.adapter = adapter
+
+ */
